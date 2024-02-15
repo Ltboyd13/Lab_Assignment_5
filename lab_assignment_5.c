96c2777b4a1060cb6468ece4d75bfd34669b9eee
LOGAN BOYD <57021782+Ltboyd13@users.noreply.github.com>
2024-02-15 14:50:26 -0500

    Replaced lab5 file with lab4 file

--- lab_assignment_5.c
+++ lab_assignment_5.c
@@ -1,61 +1,115 @@
+// Logan Boyd
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
+typedef struct node
+{
 	char letter;
-	struct node* next;
+	struct node *next;
 } node;
 
 // Returns number of nodes in the linkedList.
-int length(node* head)
+int length(node *head)
 {
-   struct node *tmp = head;
-    int len = 0;
-   while (tmp != NULL)
-   {
-      tmp = tmp->next;
-      len++;
-   }
+	node *tmp = head;
+	int counter = 0;
 
-   return (len);
+	while (tmp != NULL)
+	{
+		counter++;
+		tmp = tmp->next;
+	}
+
+	return counter;
 }
 
 // parses the string in the linkedList
 //  if the linked list is head -> |a|->|b|->|c|
 //  then toCString function wil return "abc"
-char* toCString(node* head)
+char *toCString(node *head)
 {
+	char *str = (char *)malloc((length(head) + 1) * sizeof(char));
+
+	node *tmp = head;
+	int i = 0;
+
+	while (tmp != NULL)
+	{
+		str[i] = tmp->letter;
+		tmp = tmp->next;
+		i++;
+	}
+
+	str[i] = '\0';
+	return str;
 }
 
 // inserts character to the linkedlist
 // f the linked list is head -> |a|->|b|->|c|
 // then insertChar(&head, 'x') will update the linked list as foolows:
 // head -> |a|->|b|->|c|->|x|
-void insertChar(node** pHead, char c)
+void insertChar(node **pHead, char c)
 {
+	node* newNode = (node *)malloc(sizeof(node)); // create node 
+	if (newNode == NULL)
+	{
+		return;
+	}
+	newNode->letter = c; // take in char
+	newNode->next = NULL;
+
+	if (*pHead == NULL)
+	{
+		*pHead = newNode;
+	}
+	else {
+		node* current = *pHead;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = newNode;
+	}
 }
 
 // deletes all nodes in the linkedList.
-void deleteList(node** pHead)
+void deleteList(node **pHead)
 {
+	if (*pHead == NULL) 
+	{
+		return; // the list is already empty
+	}
+	
+	node* current = *pHead;
+	node* nextNode = NULL;
+
+	while (current != NULL)
+	{
+		nextNode = current->next; // set reference to the next node
+		free(current); // free the current node
+		current = nextNode; // move to next node
+	}
+
+	*pHead = NULL; // set the head to null 
+	
 }
 
 int main(void)
 {
 	int i, strLen, numInputs;
-	node* head = NULL;
-	char* str;
+	node *head = NULL;
+	char *str;
 	char c;
-	FILE* inFile = fopen("input.txt","r");
+	FILE *inFile = fopen("input.txt", "r");
 
 	fscanf(inFile, " %d\n", &numInputs);
-	
+
 	while (numInputs-- > 0)
 	{
 		fscanf(inFile, " %d\n", &strLen);
 		for (i = 0; i < strLen; i++)
 		{
-			fscanf(inFile," %c", &c);
+			fscanf(inFile, " %c", &c);
 			insertChar(&head, c);
 		}
 
